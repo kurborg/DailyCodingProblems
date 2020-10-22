e2ce08a9dbe29427da61c5b9933a191dfd25baed
Kurt <kurborg@gmail.com>
2020-10-22 16:40:20 -0700

    Added Longest Palindrome

--- DailyCodingProblems/src/longestPalindrome.java
+++ DailyCodingProblems/src/longestPalindrome.java
@@ -8,16 +8,21 @@ class longestPalindromicSubstring {
         
         int start = 0, end = 0;
         for(int i = 0; i <s.length(); i++){
+        	System.out.println("\n\nROUND: " + i);
             int len1 = expandAroundCenter(s, i, i);
             int len2 = expandAroundCenter(s, i, i + 1);
             int len = Math.max(len1, len2);
             
+            System.out.println("LEN: " + len);
+            
             if(len > end - start) {
                 start = i - (len - 1) /2;
                 end = i + len/2;
+                System.out.println("START: " + start);
+                System.out.println("END: " + end);
             }
         }
-        return s.substring(start, end+1);
+        return s.substring(start, end+1); //NON INCLUSIVE ON END!
     }
     
     private int expandAroundCenter(String s, int left, int right){
@@ -26,6 +31,7 @@ class longestPalindromicSubstring {
             L--;
             R++;
         }
+        System.out.println("RIGHT AND LEFT: " + R + " " + L);
         return R - L - 1;
     }
 
