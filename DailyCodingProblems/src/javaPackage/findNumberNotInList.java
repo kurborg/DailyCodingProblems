060e2985b0edd76100390fb5aa92dea369f894c1
Kurt <kurborg@gmail.com>
2022-01-12 19:48:54 -0700

    Add rand Number not in list

---
+++ DailyCodingProblems/src/javaPackage/findNumberNotInList.java
@@ -0,0 +1,40 @@
+package javaPackage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class findNumberNotInList {
+    
+    static Integer findNumbers(List arr, int n)
+    {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+
+        n = (int) Math.floor(n);
+        if (n == 0)
+            return arr.contains(0) ? null : 0;
+
+        int i = 0;
+        int dir = (int) Math.signum(n);
+
+        while(i != n)
+        {
+            if (!arr.contains(i))
+                result.add(i);
+            i+= dir;
+        }
+
+        return result.get((int)Math.floor(Math.random()*result.size()));
+    }
+
+    public static void main(String args[])
+    {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(4);
+        arr.add(5);
+        int result = findNumbers(arr, 7);
+
+        System.out.println(result);
+    }
+}
